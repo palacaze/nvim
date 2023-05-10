918bd00721c9c8ef4e6e2c738433f876ddaad4b0
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2023-05-10 09:39:42 +0200

    null-ls: mention plenary dependency

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -154,7 +154,7 @@ return {
     {
         "jose-elias-alvarez/null-ls.nvim",
         event = { "BufReadPre", "BufNewFile" },
-        dependencies = { "mason.nvim" },
+        dependencies = { "mason.nvim", "plenary.nvim" },
         opts = function()
             local nls = require("null-ls")
             local b = nls.builtins
