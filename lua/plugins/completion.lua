bbecea898c4468bc27b61336bee66e82217efeb5
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2023-06-27 22:10:08 +0200

    feat: add a snippet to create comment boxes

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -35,6 +35,54 @@ return {
             -- load snippets provided by friendly-snippets and my snippets
             require("luasnip.loaders.from_vscode").lazy_load()
             require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets"  } })
+
+            local ls = require("luasnip")
+            local s = ls.snippet
+            local t = ls.text_node
+            local i = ls.insert_node
+            local f = ls.function_node
+
+            -- A Comment box snippet
+            local function box(opts)
+                local function box_width()
+                    return opts.box_width or (vim.opt.textwidth:get() > 0 and vim.opt.textwidth:get() or 80)
+                end
+                local function padding(cs, input_text)
+                    local spaces = box_width() - (2 * #cs)
+                    spaces = spaces - #input_text
+                    return spaces / 2
+                end
+                local comment_string = function()
+                    return require("luasnip.util.util").buffer_comment_chars()[1]
+                end
+
+                return {
+                    f(function()
+                        local cs = comment_string()
+                        return string.rep(string.sub(cs, 1, 1), box_width())
+                    end, { 1 }),
+                    t({ "", "" }),
+                    f(function(args)
+                        local cs = comment_string()
+                        return cs .. string.rep(" ", math.floor(padding(cs, args[1][1])))
+                    end, { 1 }),
+                    i(1, "placeholder"),
+                    f(function(args)
+                        local cs = comment_string()
+                        return string.rep(" ", math.ceil(padding(cs, args[1][1]))) .. cs
+                    end, { 1 }),
+                    t({ "", "" }),
+                    f(function()
+                        local cs = comment_string()
+                        return string.rep(string.sub(cs, 1, 1), box_width())
+                    end, { 1 }),
+                }
+            end
+
+            ls.add_snippets("all", {
+                s({ trig = "box" }, box({ box_width = 24 })),
+                s({ trig = "bbox" }, box({})),
+            })
         end,
     },
 
