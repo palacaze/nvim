f9117d29c1a993f355527dd74eb31516d9db567e
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2023-07-06 15:53:38 +0200

    refactor: "modularize" lsp mappings

--- lua/plugins/lsp/mappings.lua
+++ lua/plugins/lsp/mappings.lua
@@ -1,31 +1,5 @@
 local M = {}
 
--- Simple implementation of highlighting of the current variable and its usages
--- if navigator.lua is missing.
-local function highlight_symbols(bufnr)
-    vim.cmd([[
-        hi! link LspReferenceRead Visual
-        hi! link LspReferenceText Visual
-        hi! link LspReferenceWrite Visual
-    ]])
-
-    local gid = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
-    vim.api.nvim_clear_autocmds({
-        group = gid,
-        buffer = bufnr,
-    })
-    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        group = gid,
-        buffer = bufnr,
-        callback = vim.lsp.buf.document_highlight,
-    })
-    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-        group = gid,
-        buffer = bufnr,
-        callback = vim.lsp.buf.clear_references,
-    })
-end
-
 -- Toggle diagnostics
 local function toggle_diagnostics()
     if vim.diagnostic.is_disabled(0) then
@@ -35,13 +9,20 @@ local function toggle_diagnostics()
     end
 end
 
--- Toggle diagnostics
+-- Toggle virtual diagnostics
 local function toggle_virtual_diagnostics()
     local cfg = vim.diagnostic.config()
     if cfg.virtual_text and cfg.virtual_text == true then
         vim.diagnostic.config({ virtual_text = false })
     else
         vim.diagnostic.config({ virtual_text = true })
+    end
+end
+
+-- Toggle inlay hints
+local function toggle_inlay_hints()
+    if vim.lsp.inlay_hint then
+        vim.lsp.inlay_hint(0, nil)
     end
 end
 
@@ -56,236 +37,152 @@ local function pick(method)
 
     local tel = require("telescope.builtin")
     return function()
-        local theme = require("telescope.themes").get_dropdown()
+        local theme = require("telescope.themes").get_ivy()
         tel[method](theme)
     end
 end
 
-function M.setup_native(client, bufnr)
-    local with_picker, picker = pcall(require, "fzf-lua")
-    if not with_picker then
-        with_picker, picker = pcall(require, "telescope.builtin")
-    end
-    local caps = client.server_capabilities
+local config = {
+    prev_diagnostic = { desc = "Go to previous diagnostic", keys = { "<S-F2>", "<F14>", "[e" } },
+    next_diagnostic = { desc = "Go to next diagnostic", keys = { "<S-F3", "<F15>", "]e" } },
+    prev_error = { desc = "Go to previous error", keys = {"[E"} },
+    next_error = { desc = "Go to next error", keys = {"]E"} },
+    set_loclist = { desc = "Fill loclist with diagnostics", keys = {"<Leader>lq"} },
+    hover_diagnostic = { desc = "Hover diagnostic", keys = {"<M-d>"} },
+    toggle_diagnostics = { desc = "Toggle diagnostics", keys = {"<Leader>lt"}  },
+    toggle_virtual_diagnostics = { desc = "Toggle diagnostics (virtual text)", keys = {"<leader>lT"} },
+    declaration = { desc = "Jump to declaration", keys = {"gD"}, cap = "declarationProvider" },
+    definition = { desc = "Jump do definition", keys = {"gd"}, cap = "definitionProvider" },
+    references = { desc = "Display references", keys = { "gr", "<Leader>lR" }, cap = "referencesProvider" },
+    type_definition = { desc = "Display type definition", keys = {"gT"}, cap = "typeDefinitionProvider" },
+    implementations = { desc = "List implementations", keys = {"<Leader>li"}, cap = "implementationProvider" },
+    code_action = { desc = "Execute code action", keys = { "<Leader>la", "<M-a>" }, cap = "codeActionProvider" },
+    incoming_calls = { desc = "Incoming calls", keys = {"<Leader>lI"}, cap = "callHierarchyProvider" },
+    outgoing_calls = { desc = "Outgoing calls", keys = {"<Leader>lO"}, cap = "callHierarchyProvider" },
+    document_symbols = { desc = "Document symbols", keys = {"<Leader>ls"}, cap = "documentSymbolProvider" },
+    workspace_symbols = { desc = "Workspace symbols", keys = {"<Leader>lS"}, cap = "workspaceSymbolProvider" },
+    rename = { desc = "Rename Symbol", keys = {"<Leader>lr"}, cap = "renameProvider" },
+    signature_help = { desc = "Display symbol signature", keys = {"<Leader>lh"}, cap = "signatureHelpProvider" },
+    format = { desc = "Format code", keys = { "<Leader>lf", "<Leader>F" }, mode = { "n", "v" }, cap = "documentFormattingProvider" },
+    hover_help = { desc = "symbol information", keys = {"K"}, cap = "hoverProvider" },
+    toggle_inlay_hints = { desc = "Toggle inlay hints", keys = { "<Leader>lH" }, cap = "inlayHintProvider" },
+    outline = { desc = "Display symbols outline", keys = {"<Leader>lo"} },
+}
 
-    -- Mappings
-    local lmap = function(mode, keys, func, desc)
-        if desc then
-            desc = "LSP: " .. desc
+local native_proto = {
+    prev_diagnostic = vim.diagnostic.goto_prev,
+    next_diagnostic = vim.diagnostic.goto_next,
+    prev_error = function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end,
+    next_error = function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end,
+    set_loclist = vim.diagnostic.setloclist,
+    hover_diagnostic = vim.diagnostic.open_float,
+    toggle_diagnostics = toggle_diagnostics,
+    toggle_virtual_diagnostics = toggle_virtual_diagnostics,
+    declaration = vim.lsp.buf.declaration,
+    definition = vim.lsp.buf.definition,
+    references = vim.lsp.buf.references,
+    type_definition = vim.lsp.buf.type_definition,
+    implementations = vim.lsp.buf.implementation,
+    code_action = vim.lsp.buf.code_action,
+    incoming_calls = vim.lsp.buf.incoming_calls,
+    outgoing_calls = vim.lsp.buf.outgoing_calls,
+    document_symbols = vim.lsp.buf.document_symbol,
+    workspace_symbols = vim.lsp.buf.workspace_symbol,
+    rename = vim.lsp.buf.rename,
+    signature_help = vim.lsp.buf.signature_help,
+    format = vim.lsp.buf.format,
+    hover_help = vim.lsp.buf.hover,
+    toggle_inlay_hints = toggle_inlay_hints,
+}
+
+local picker_proto = {
+    definition = pick("lsp_definitions"),
+    references = pick("lsp_references"),
+    implementations = pick("lsp_implementations"),
+    incoming_calls = pick("lsp_incoming_calls"),
+    outgoing_calls = pick("lsp_outgoing_calls"),
+    document_symbols = pick("lsp_document_symbols"),
+    workspace_symbols = pick("lsp_workspace_symbols"),
+}
+
+local saga_proto = {
+    prev_diagnostic = "<Cmd>Lspsaga diagnostic_jump_prev<CR>",
+    next_diagnostic = "<Cmd>Lspsaga diagnostic_jump_next<CR>",
+    prev_error = function() require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR }) end,
+    next_error = function() require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR }) end,
+    hover_diagnostic = "<Cmd>Lspsaga show_line_diagnostics ++unfocus<CR>",
+    definition = "<Cmd>Lspsaga peek_definition<CR>",
+    references = "<Cmd>Lspsaga lsp_finder<CR>",
+    code_action = "<Cmd>Lspsaga code_action<CR>",
+    incoming_calls = "<cmd>Lspsaga incoming_calls<CR>",
+    outgoing_calls = "<cmd>Lspsaga outgoing_calls<CR>",
+    rename = "<Cmd>Lspsaga rename<CR>",
+    hover_help = "<Cmd>Lspsaga hover_doc ++keep<CR>",
+    outline = "<Cmd>Lspsaga outline<CR>",
+}
+
+function M.setup()
+    -- Configure the functions to use once
+    if M.config then
+        return
+    end
+
+    M.config = config
+
+    -- Assign implementations for each feature, if not already setup by another
+    -- implementation in a previous call to add_functions.
+    local add_functions = function(proto)
+        for feat, func in pairs(proto) do
+            if not M.config[feat].func then
+                M.config[feat].func = func
+            end
         end
-        local opts = { noremap = true, silent = true, nowait = true, buffer = bufnr, desc = desc }
-        vim.keymap.set(mode, keys, func, opts)
     end
 
-    lmap("n", "<S-F2>", vim.diagnostic.goto_prev, "Go to previous diagnostic")
-    lmap("n", "<F14>", vim.diagnostic.goto_prev, "Go to previous diagnostic")
-    lmap("n", "<S-F3>", vim.diagnostic.goto_next, "Go to next diagnostic")
-    lmap("n", "<F15>", vim.diagnostic.goto_next, "Go to next diagnostic")
-
-    lmap("n", "<M-d>", vim.diagnostic.open_float, "Hover diagnostics")
-    lmap("n", "<leader>lq", vim.diagnostic.setloclist, "Fill loclist with diagnostics")
-    lmap("n", "<leader>lt", toggle_diagnostics, "Toggle diagnostics")
-    lmap("n", "<leader>lT", toggle_virtual_diagnostics, "Toggle virtual diagnostics")
-
-    if caps.declarationProvider then
-        lmap("n", "gD", vim.lsp.buf.declaration, "Go to symbol declaration")
+    -- lspsage if available
+    local with_saga, _ = pcall(require, "lspsaga")
+    if with_saga then
+        add_functions(saga_proto)
     end
 
-    if caps.definitionProvider then
-        if with_picker then
-            lmap("n", "gd", pick("lsp_definitions"), "Go to symbol definition")
-        else
-            lmap("n", "gd", vim.lsp.buf.definition, "Go to symbol definition")
-        end
+    -- telescope if available
+    local with_picker, _ = pcall(require, "telescope.builtin")
+    if with_picker then
+        add_functions(picker_proto)
     end
 
-    if caps.referencesProvider then
-        if with_picker then
-            lmap("n", "gr", pick("lsp_references"), "Find references to symbol")
-            lmap("n", "<leader>lR", pick("lsp_references"), "Find references to symbol")
-        else
-            lmap("n", "gr", vim.lsp.buf.references, "Find references to symbol")
-            lmap("n", "<leader>lR", vim.lsp.buf.references, "Find references to symbol")
-        end
-    end
-
-    if caps.typeDefinitionProvider then
-        lmap("n", "gT", vim.lsp.buf.type_definition, "Display symbol type definition")
-    end
-
-    if caps.implementationProvider then
-        if with_picker then
-            lmap("n", "gi", pick("lsp_implementations"), "List Implementations")
-        else
-            lmap("n", "gi", vim.lsp.buf.implementation, "List Implementations")
-        end
-    end
-
-    if caps.codeActionProvider then
-        lmap({ "n", "x" }, "<leader>la", vim.lsp.buf.code_action, "Execute code action")
-    end
-
-    if caps.callHierarchyProvider then
-        if with_picker then
-            lmap("n", "<leader>lI", pick("lsp_incoming_calls"), "Incoming calls")
-            lmap("n", "<leader>lO", pick("lsp_outgoing_calls"), "Outgoing calls")
-        else
-            lmap("n", "<leader>lI", vim.lsp.buf.incoming_calls, "Incoming calls")
-            lmap("n", "<leader>lO", vim.lsp.buf.outgoing_calls, "Outgoing calls")
-        end
-    end
-
-    if caps.documentSymbolProvider then
-        if with_picker then
-            lmap("n", "<leader>ls", pick("lsp_document_symbols"), "Document symbols")
-        else
-            lmap("n", "<leader>ls", vim.lsp.buf.document_symbol, "Document symbol")
-        end
-    end
-
-    if caps.workspaceSymbolProvider then
-        if with_picker then
-            lmap("n", "<leader>lS", pick("lsp_workspace_symbols"), "Workspace symbols")
-        else
-            lmap("n", "<leader>lS", vim.lsp.buf.workspace_symbol, "Workspace symbols")
-        end
-    end
-
-    if caps.renameProvider then
-        lmap("n", "<leader>lr", vim.lsp.buf.rename, "Rename symbol")
-    end
-
-    if caps.signatureHelpProvider then
-        lmap("n", "<leader>lh", vim.lsp.buf.signature_help, "Display symbol signature")
-    end
-
-    if caps.documentFormattingProvider then
-        lmap({"n", "v"}, "<leader>lf", vim.lsp.buf.format, "Format code")
-        lmap({"n", "v"}, "<leader>F", vim.lsp.buf.format, "Format code")
-    end
-
-    if caps.hoverProvider then
-        lmap("n", "K", vim.lsp.buf.hover, "Hover symbol")
-    end
-
-    -- if caps.documentHighlightProvider then
-        -- highlight_symbols(bufnr)
-    -- end
+    -- defaults
+    add_functions(native_proto)
 end
 
--- create LSP key mappings for the given buffer and lspsaga.lua
-function M.setup_lspsaga(client, bufnr)
-    local with_picker, picker = pcall(require, "telescope.builtin")
-    local caps = client.server_capabilities
-
-    -- Mappings
-    local lmap = function(mode, keys, func, desc)
-        if desc then
-            desc = "LSP: " .. desc
-        end
-        local opts = { noremap = true, silent = true, nowait = true, buffer = bufnr, desc = desc }
-        vim.keymap.set(mode, keys, func, opts)
+function M.map(bufnr, cfg)
+    if not cfg.func then
+        return
     end
 
-    lmap("n", "<S-F2>", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", "Go to previous diagnostic")
-    lmap("n", "<S-F3>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", "Go to next diagnostic")
-    lmap("n", "[e", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", "Go to previous diagnostic")
-    lmap("n", "]e", "<Cmd>Lspsaga diagnostic_jump_next<CR>", "Go to next diagnostic")
-    -- Only jump to error
-    lmap("n", "[E", function()
-        require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
-    end, "Go to previous error")
-    lmap("n", "]E", function()
-        require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
-    end, "Go to next error")
-
-    -- lmap("n", "<M-d>", vim.diagnostic.open_float, "Hover diagnostics")
-    lmap("n", "<M-d>", "<Cmd>Lspsaga show_line_diagnostics<CR>", "Hover diagnostics")
-    lmap("n", "<leader>lq", vim.diagnostic.setloclist, "Fill loclist with diagnostics")
-    lmap("n", "<leader>lt", toggle_diagnostics, "Toggle diagnostics")
-    lmap("n", "<leader>lT", toggle_virtual_diagnostics, "Toggle virtual diagnostics")
-
-    if caps.declarationProvider then
-        lmap("n", "gD", vim.lsp.buf.declaration, "Go to symbol declaration")
+    for _, k in ipairs(cfg.keys) do
+        vim.keymap.set(cfg.mode or "n", k, cfg.func, {
+            noremap = true,
+            silent = true,
+            nowait = true,
+            buffer = bufnr,
+            desc = cfg.desc
+        })
     end
-
-    if caps.definitionProvider then
-        lmap("n", "gd", "<Cmd>Lspsaga peek_definition<CR>", "Display symbol definition")
-    end
-
-    if caps.referencesProvider then
-        lmap("n", "gr", "<Cmd>Lspsaga lsp_finder<CR>", "Find references to symbol")
-        lmap("n", "<Leader>lR", "<Cmd>Lspsaga lsp_finder<CR>", "Find references to symbol")
-    end
-
-    if caps.typeDefinitionProvider then
-        lmap("n", "gT", vim.lsp.buf.type_definition, "Display symbol type definition")
-    end
-
-    if caps.implementationProvider then
-        if with_picker then
-            lmap("n", "gi", pick("lsp_implementations"), "List Implementations")
-        else
-            lmap("n", "gi", vim.lsp.buf.implementation, "List Implementations")
-        end
-    end
-
-    if caps.codeActionProvider then
-        lmap({"n", "v"}, "<leader>la", "<Cmd>Lspsaga code_action<CR>", "Execute code action")
-    end
-
-    if caps.callHierarchyProvider then
-        lmap("n", "<Leader>lI", "<cmd>Lspsaga incoming_calls<CR>", "Incoming calls")
-        lmap("n", "<Leader>lO", "<cmd>Lspsaga outgoing_calls<CR>", "Outgoing calls")
-    end
-
-    if caps.documentSymbolProvider then
-        if with_picker then
-            lmap("n", "<leader>ls", pick("lsp_document_symbols"), "Document symbols")
-        else
-            lmap("n", "<leader>ls", vim.lsp.buf.document_symbol, "Document symbol")
-        end
-    end
-
-    if caps.workspaceSymbolProvider then
-        if with_picker then
-            lmap("n", "<leader>lS", pick("lsp_workspace_symbols"), "Workspace symbols")
-        else
-            lmap("n", "<leader>lS", vim.lsp.buf.workspace_symbol, "Workspace symbols")
-        end
-    end
-
-    if caps.renameProvider then
-        lmap("n", "<leader>lr", "<Cmd>Lspsaga rename<CR>", "Rename symbol")
-    end
-
-    if caps.signatureHelpProvider then
-        lmap("n", "<leader>lh", vim.lsp.buf.signature_help, "Display symbol signature")
-    end
-
-    if caps.documentFormattingProvider then
-        lmap({"n", "v"}, "<leader>lf", vim.lsp.buf.format, "Format code")
-        lmap({"n", "v"}, "<leader>F", vim.lsp.buf.format, "Format code")
-    end
-
-    if caps.hoverProvider then
-        lmap("n", "K", "<Cmd>Lspsaga hover_doc<CR>", "Hover symbol")
-    end
-
-    -- if caps.documentHighlightProvider then
-        -- highlight_symbols(bufnr)
-    -- end
-
-    -- Outline
-    lmap("n", "<Leader>lo", "<Cmd>Lspsaga outline<CR>", "Display symbols outline")
 end
 
 function M.on_attach(client, bufnr)
-    local have_lspsaga, _ = pcall(require, "lspsaga")
-    if have_lspsaga then
-        M.setup_lspsaga(client, bufnr)
-    else
-        M.setup_native(client, bufnr)
+    -- Configure once
+    if not M.config then
+        M.setup()
+    end
+
+    local caps = client.server_capabilities
+
+    for _, cfg in pairs(config) do
+        if not cfg.cap or (caps[cfg.cap] and caps[cfg.cap] == true) then
+            M.map(bufnr, cfg)
+        end
     end
 end
 
