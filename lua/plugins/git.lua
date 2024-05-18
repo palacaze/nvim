22da0418ef3cae2a6e029ca1385cf9946cf6fe2f
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2024-05-18 16:56:08 +0200

    feat: improve the look of diffs in diffview

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -95,6 +95,21 @@ return {
                     },
                 },
                 hooks = {
+                    -- color diff like github
+                    diff_buf_win_enter = function(bufnr, winid, ctx)
+                        if ctx.layout_name:match("^diff2") then
+                            if ctx.symbol == "a" then
+                                vim.opt_local.winhl = table.concat({
+                                    "DiffAdd:DiffviewDiffAddAsDelete",
+                                    "DiffDelete:DiffviewDiffDelete",
+                                }, ",")
+                            elseif ctx.symbol == "b" then
+                                vim.opt_local.winhl = table.concat({
+                                    "DiffDelete:DiffviewDiffDelete",
+                                }, ",")
+                            end
+                        end
+                    end,
                     diff_buf_read = function(_)
                         if vim.fn.exists(":IBLDisable") > 0 then
                             vim.cmd("IBLDisable")
