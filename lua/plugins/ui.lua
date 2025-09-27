319c6f69077687c8d18e33e9805e3dc8afe20dac
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2025-09-27 23:32:59 +0200

    feat: add quicker plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -139,6 +139,35 @@ return {
         end,
     },
 
+    -- Improved quickfix UI
+    {
+        "stevearc/quicker.nvim",
+        event = "FileType qf",
+        keys = {
+            { "<Leader>q", function() require("quicker").toggle() end, desc = "Toggle quickfix" },
+        },
+        ---@module "quicker"
+        ---@type quicker.SetupOptions
+        opts = {
+            keys = {
+                {
+                    ">",
+                    function()
+                        require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+                    end,
+                    desc = "Expand quickfix context",
+                },
+                {
+                    "<",
+                    function()
+                        require("quicker").collapse()
+                    end,
+                    desc = "Collapse quickfix context",
+                },
+            },
+        },
+    },
+
     -- Get a list of URLs to pick from
     {
         "axieax/urlview.nvim",
