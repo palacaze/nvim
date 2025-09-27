f31f72d9416f3d8f2d57479c9fbfdabc153c6e88
Pierre-Antoine Lacaze <pal.dev@pa-l.fr>
2025-09-27 23:29:52 +0200

    feat: update venv-selector

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -17,20 +17,22 @@ return {
 
     {
         "linux-cultist/venv-selector.nvim",
-        branch = "regexp",
         lazy = true,
-        dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+        dependencies = {
+            "ibhagwan/fzf-lua",
+            "neovim/nvim-lspconfig",
+            "mfussenegger/nvim-dap-python"
+        },
         opts = {
             options = {
+                picker = "fzf-lua",
                 activate_venv_in_terminal = true,
                 set_environment_variables = true,
             },
         },
         keys = {
             -- Keymap to open VenvSelector to pick a venv.
-            { "<leader>vs", "<cmd>VenvSelect<cr>" },
-            -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-            { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+            { "<leader>v", "<cmd>VenvSelect<cr>", desc = "Select Python virtual env" },
         },
     },
 
